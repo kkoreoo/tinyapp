bf81432eee23255c912f1ad172cb60aaa5879010
atle <alextle24@gmail.com>
2024-01-22 19:24:45 -0700

    created a post route to retrieve a longURL fomr the forum in url/new

--- express_server.js
+++ express_server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080; 
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString () {
+  
 };
 
 app.get("/", (req, res) => {
@@ -17,6 +23,15 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Response with 'Ok' (We will replace this)
+})
 
 app.get("/urls/:id", (req, res) => {
   const shortUrl = req.params.id;
