f9719446d3135045e8667fee38906cdbabacc161
atle <alextle24@gmail.com>
2024-01-23 16:19:30 -0700

    created an edit button, which would allow users to edit the long URl associated with the shorturl

--- express_server.js
+++ express_server.js
@@ -11,6 +11,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//short ID generator
 function generateRandomString () {
   let shortID = '';
   let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -21,48 +22,56 @@ function generateRandomString () {
   return shortID;
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+// READ ///  Displays all the URLs in the database
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// ADD /// Page to enter a new url;
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// CREATE /// New Url added will redirect to show only that url in our website
 app.post("/urls", (req, res) => {
   shortID = generateRandomString();
   urlDatabase[shortID] = req.body.longURL 
   res.redirect(`/urls/${shortID}`);
 });
 
+
+// READ /// Will redirect user to urls unique page to edit
+app.get("/urls/:id/edit", (req, res) => {
+  const shortUrl = req.params.id;
+  res.redirect(`/urls/${shortUrl}`);
+});
+
+// UPDATE // Upon submitting the new long url, will replace existing one.
+app.post("/urls/:id/", (req, res) => {
+  const shortUrl = req.params.id;
+  urlDatabase[shortUrl] = req.body.longURL;
+  res.redirect(`/urls/`);
+});
+
+// READ /// When clicked on the short url -> redirects to the listed website
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
-})
+});
 
+// DETELE /// Will delete the selected url and return to the /urls page
 app.post("/urls/:id/delete", (req, res) => {
   const shortUrl = req.params.id;
   delete urlDatabase[shortUrl];
   res.redirect("/urls");
-})
-
-// app.get("/urls/:id", (req, res) => {
-//   const shortUrl = req.params.id;
-//   const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] }
-//   res.render("urls_show", templateVars);
-// });
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// READ /// Will show the individual URL in it's own page.
+app.get("/urls/:id", (req, res) => {
+  const shortUrl = req.params.id;
+  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] }
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
